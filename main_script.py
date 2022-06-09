3ab9de49019d8a845f67de1d0d763aea7c66a55d
bysuses <rebusik67@gmail.com>
2022-06-09 23:59:53 +0200

    gameboard gets created

--- main_script.py
+++ main_script.py
@@ -6,10 +6,14 @@ import Player_menu
 array_size = 0
 game_speed = 0
 gameboard = 0
-#gameboard = numpy.array(array_size*[array_size*['OO']])
+
+#menu interaction
 menu = Player_menu.Menu()
 menu.display_menu_and_get_data()
 game_speed = menu.get_game_speed()
 array_size = menu.get_array_size()
-#numpy.savetxt(sys.stdout, gameboard, fmt="%.2s")
+
+#defining the gameboard
+gameboard = numpy.array(array_size*[array_size*['OO']])
+numpy.savetxt(sys.stdout, gameboard, fmt="%.2s")
 
